632d1a37d11b1f51a8f74ca3a3fe509272cb792c
Justin Appelgren <justinappelgren@gmail.com>
2020-12-18 16:08:43 -0600

    Sourced and created  all files.  Set up server

---
+++ server/public/scripts/index.js
@@ -0,0 +1,1 @@
+console.log(`%cYou're hear`, `color: tomato`)
---
+++ server/server.js
@@ -0,0 +1,10 @@
+const express = require('express');
+
+const PORT = 5000;
+const app = express();
+
+app.use(express.static('server/public'))
+
+app.listen(PORT, () => {
+    console.log(`Server started on port ${PORT}`)
+})
