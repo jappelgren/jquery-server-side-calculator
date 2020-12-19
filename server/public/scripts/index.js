0a87339ad6ef4c704da560055bc1a2f0fef1603c
Justin Appelgren <justinappelgren@gmail.com>
2020-12-18 18:08:41 -0600

    Added all calculator functionality.  Server does all math and sends back to client.js

--- server/public/scripts/index.js
+++ server/public/scripts/index.js
@@ -1,1 +1,45 @@
-console.log(`%cYou're hear`, `color: tomato`)
+$(document).ready(handleReady);
+
+function handleReady() {
+    $(document).on('click', '.operator-btn', assignOperator);
+    $(document).on('click', '#equals-btn', createMathObj)
+}
+
+let operatorIn;
+
+let mathObj = {};
+
+function assignOperator() {
+    operatorIn = $(this).data('btn');
+};
+
+function createMathObj() {
+    mathObj = {
+        numOne: Number($('#first-number').val()),
+        operator: operatorIn,
+        numTwo: Number($('#second-number').val())
+    }
+    console.log(mathObj)
+    mathSubmit()
+};
+
+function mathSubmit() {
+    $.ajax({
+        url: '/calculate',
+        type: 'POST',
+        data: mathObj
+    }).then(function (response) {
+        console.log(response)
+        getAnswer()
+    })
+
+}
+
+function getAnswer() {
+    $.ajax({
+        url: '/calculate',
+        type: 'GET'
+    }).then(function (response) {
+        console.log(response)
+    })
+}
--- server/server.js
+++ server/server.js
@@ -1,9 +1,41 @@
 const express = require('express');
+const bodyParser = require('body-parser')
 
 const PORT = 5000;
 const app = express();
 
+let history = []
+let answer;
+
 app.use(express.static('server/public'))
+app.use(bodyParser.urlencoded({ extended: true }))
+
+app.post('/calculate', (req, res) => {
+    history.push(req.body);
+
+    let firstInput = Number(req.body.numOne);
+    let operator = req.body.operator;
+    let secondInput = Number(req.body.numTwo);
+
+    if (operator == '+') {
+        answer = (firstInput + secondInput).toString()
+    } else if (operator == '-') {
+        answer = (firstInput - secondInput).toString()
+    } else if (operator == '*') {
+        answer = (firstInput * secondInput).toString()
+    } else if (operator == '/') {
+        answer = (firstInput / secondInput).toString()
+    }
+
+
+    res.sendStatus(200)
+})
+
+app.get('/calculate', (req, res) => {
+    res.send(answer)
+})
+
+
 
 app.listen(PORT, () => {
     console.log(`Server started on port ${PORT}`)
